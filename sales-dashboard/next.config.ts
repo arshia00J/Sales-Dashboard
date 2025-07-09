1c8a50703a6eaeeec06b025dc93edd7255bcc0fd
Arshia Jafari <arshia00iq@proton.me>
2025-07-09 12:05:59 +0330

    update next config

--- sales-dashboard/next.config.ts
+++ sales-dashboard/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/your-repo-name', // ⚠️ Required for GitHub Pages
+  basePath: '/Sales-Dashboard/sales-dashboard',
 };
 
 module.exports = nextConfig;
