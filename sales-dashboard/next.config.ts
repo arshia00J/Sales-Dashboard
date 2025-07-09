236e2ae101b1de030e87fe0ce407301c961337f3
Arshia Jafari <arshia00iq@proton.me>
2025-07-09 12:55:25 +0330

    update config

--- sales-dashboard/next.config.ts
+++ sales-dashboard/next.config.ts
@@ -1,11 +1,7 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/Sales-Dashboard/sales-dashboard',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
