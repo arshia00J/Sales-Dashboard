6219123983457f1ff3b7a12eb0c188da8d38f4e4
Arshia <139552811+arshia00J@users.noreply.github.com>
2025-07-11 22:19:50 +0330

    Update and rename next.config.ts to next.config.js

---
+++ sales-dashboard/next.config.js
@@ -0,0 +1,9 @@
+const isGithubPages = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  basePath: isGithubPages ? "/Sales-Dashboard" : "",
+  assetPrefix: isGithubPages ? "/Sales-Dashboard/" : "",
+};
+
+module.exports = nextConfig;
