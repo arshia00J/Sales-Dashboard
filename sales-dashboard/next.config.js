2ac43ea9e3c9ec58bc71c0bbf7b1771792b345ff
Arshia Jafari <arshia00iq@proton.me>
2025-07-12 14:24:36 +0330

    fix

--- sales-dashboard/next.config.js
+++ sales-dashboard/next.config.js
@@ -3,7 +3,8 @@ const isGithubPages = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   basePath: isGithubPages ? "/Sales-Dashboard" : "",
-  assetPrefix: isGithubPages ? "/Sales-Dashboard/" : ""
+  assetPrefix: isGithubPages ? "/Sales-Dashboard/" : "",
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
